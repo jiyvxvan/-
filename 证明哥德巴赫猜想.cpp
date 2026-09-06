b0307ca4f247fdf151a8a7d1e819fbad6efbe23a
jiyvxvan <2537769683@qq.com>
2026-09-06 21:01:33 +0800

    Add files via upload

---
+++ 证明哥德巴赫猜想.cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int zhi(int x)
+{
+    if(x<2)return 0;
+    for(int i=2;i*i<=x;i++)
+    {
+        if(x%i==0)return 0;
+        
+    }
+    return 1;
+}
+
+
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=4;i<=n;i+=2)
+    {
+       for(int j=2;j<=i/2;j++)
+       {
+           if(zhi(j))
+           {
+               int q=i-j;
+               if(zhi(q))
+               {
+                   printf("%d=%d+%d\n",i,j,q);
+                   break;
+               }
+           }
+       }
+
+
+
+        
+    }
+
+
+
+
+
+    
+    return 0;
+}
