ca9698a3cfd912db95245f05f749070c59b1bc2a
jiyvxvan <2537769683@qq.com>
2026-09-07 21:54:57 +0800

    Add files via upload

--- 未命名1.cpp
+++ 未命名1.cpp
@@ -1,55 +1,8 @@
 #include<stdio.h>
-void s(int a[],int x,int y)
+int main() 
 {
-    int t=a[y];
-    for(int i=y;i>=x+1;i--)
-    {
-        a[i]=a[i-1];
-    }
-    a[x]=t;
-}
-void sb(int a[],int x,int y)
-{
-     int t=a[x];
-    for(int i=x;i<=y-1;i++)
-    {
-        a[i]=a[i+1];
-    }
-    a[y]=t;
-}
-void p(int a[],int n)
-{
-    for(int i=0;i<n;i++)
-    {
-        printf("%d ",a[i]);
-    }
-    printf("\n");
-}
-void z(int a[],int x,int y)
-{
-    if(x==y)
-    {
-        p(a,y+1);
-        return;
-    }
-    for(int i=x;i<=y;i++)
-    {
-        s(a,x,i);
-        z(a,x+1,y);
-        sb(a,x,i);
-    }
-}
+     char i='¶¡Ïþ¾ý'£» 
 
-int main()
-{
-    int n;
-    scanf("%d",&n);
-    int a[n];
-    for(int i=0;i<n;i++)
-    {
-        a[i]=i+1;
-    }
-    z(a,0,n-1);
-    
+
     return 0;
 }
